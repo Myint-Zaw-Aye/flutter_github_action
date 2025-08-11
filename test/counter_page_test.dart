72932859c0cefe47d992201126ef3ae5bfc7e14b
WinterCoder <52817033+Myint-Zaw-Aye@users.noreply.github.com>
2025-08-12 00:06:06 +0630

    test counter for fail

--- test/counter_page_test.dart
+++ test/counter_page_test.dart
@@ -8,7 +8,7 @@ void main() {
     await tester.pumpWidget(MaterialApp(home: const CounterPage()));
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
